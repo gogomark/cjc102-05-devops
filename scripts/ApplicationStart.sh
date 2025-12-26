080d74f8d81a19cfb8aa4cc6a0929fb151c4404e
gogomark <gogogomark@gmail.com>
2025-12-26 12:54:37 +0800

    ver5.0 first commit

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run -p 5000:5000 --name flask-devops -d 129729052534.dkr.ecr.ap-northeast-1.amazonaws.com/iii-devops-ecr:latest
+docker run -p 80:5000 --name flask-web -d 751849297539.dkr.ecr.ap-northeast-1.amazonaws.com/cjc102-05-ecr-repo:latest
+sleep 20s
